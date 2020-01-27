ccd7ebc88fb82db1e2e89f75e9c0ed02449350d5
ender <1991455223@qq.com>
2020-01-27 16:23:53 +0800

    Basic

---
+++ config/site_config.php
@@ -0,0 +1,17 @@
+<?php
+//Site Settings
+define("URL", "https://aiim.ren/");
+define("API", "https://api.aiim.ren/");
+//DB Settings
+define("DB_HOST", "sql.m59.vhostgo.com");
+define("DB_NAME", "ankalau");
+define("DB_USERNAME", "ankalau");
+define("DB_PASSWORD", "7mt!qhMbw@52");
+//Email Settings
+
+
+
+//SMS Settings
+define("SMS_USERNAME", "N6104448");
+define("SMS_PASSWORD", "hQ5RrPB1Xj68a4");
+define("SMS_API", "http://smsbj1.253.com/msg/send/json");
