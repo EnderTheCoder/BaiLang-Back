93e52807fc4db180e073cf50da09b4762c0f5f81
ender <1991455223@qq.com>
2020-02-18 12:09:29 +0800

    更新了关于签名算法的README

--- test.php
+++ test.php
@@ -1,19 +1,4 @@
 <?php
 require 'Header.php';
-$params = array(
-    'sql' => 'SELECT MobilePhone,Passwords,Email FROM Users WHERE MobilePhone = \'18208157618\'',
-    'timestamp' => 1581961380,
-    'app_id' => 1,
-);
-ksort($params);
-$str = '';
-foreach ($params as $key => $value) {
-    if ($value !== '') {
-        $str .= $key.'='.     rawurlencode($value).'&';
-    }
-}
-$str .= 'app_key='.'bbc9a26ded02af12866784448a8cb0e5';
-var_dump($str);
-var_dump(md5('app_id=1&sql=SELECT%20MobilePhone%2CPasswords%2CEmail%20FROM%20Users%20WHERE%20MobilePhone%20%3D%20%2718208157618%27&timestamp=1581961380&app_key=bbc9a26ded02af12866784448a8cb0e5'));
-$sign = strtoupper(md5($str));
-stdJqReturn($sign);
+var_dump(strtoupper(md5('app_id=1&key1=%E6%88%91%E6%98%AF%E4%B8%80%E4%B8%AA%E5%8F%82%E6%95%B0&key2=%E6%88%91%E4%B9%9F%E6%98%AF%E4%B8%80%E4%B8%AA%E5%8F%82%E6%95%B0&timestamp=1581952284&app_key=1q2w3e4r5t'
+)));
